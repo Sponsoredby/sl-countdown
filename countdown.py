719860d136e6249ed7381d9dd782230f357bc3ef
Sponsoredby <74147554+Sponsoredby@users.noreply.github.com>
2020-11-18 13:38:27 +0000

    Add files via upload

---
+++ countdown.py
@@ -0,0 +1,10 @@
+#set the variable number
+number = 10
+
+#set while loop until variable is 0
+while number > 0:
+	print (number)
+	number = number - 1
+
+#print when loop is complete
+print ('Takeoff')
